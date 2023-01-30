13d445ae2b920351e6f3258c4d5e40ec0044fe29
tiagocarvalhocapgemini <tiago.carvalho@capgemini.com>
2023-01-30 15:40:51 +0100

    teste9

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(name = "src",
-      author="Tiago Carvalho",
+      author="tiagocarvalhocapgemini",
+      author_email="tiago.carvalho@capgemini.com",
       version="1.0", 
       packages = find_packages(), 
       setup_requires=['wheel'])
